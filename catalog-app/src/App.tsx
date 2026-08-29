e29772bd8c93be84b2a5289de21f36584dfca679
Marian <pirvan.marian@gmail.com>
2026-08-29 12:19:52 +0300

    Remove sources footer line from the catalog page

--- catalog-app/src/App.tsx
+++ catalog-app/src/App.tsx
@@ -2,7 +2,6 @@ import { TopBar } from './components/TopBar'
 import { Header } from './components/Header'
 import { BillOfMaterials } from './components/BillOfMaterials'
 import { GroupSection } from './components/GroupSection'
-import { Footer } from './components/Footer'
 import { catalogGroups } from './data/catalog'
 
 export default function App() {
@@ -15,7 +14,6 @@ export default function App() {
         {catalogGroups.map((group) => (
           <GroupSection key={group.tag} group={group} />
         ))}
-        <Footer />
       </div>
     </>
   )
--- catalog-app/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer>
-      Sources: F:\SassFactory\crm3-micro\packages\shared &middot; F:\leadMarketing &middot; F:\scraper
-    </footer>
-  )
-}
